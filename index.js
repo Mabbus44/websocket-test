c754ca101f880a25e49682e7ab924434bd0911b1
Mabbus44 <rasun44@hotmail.com>
2021-03-27 21:00:39 +0100

    Websocket

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const socketIO = require('socket.io')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 const { Pool } = require('pg');
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
@@ -9,9 +10,17 @@ const pool = new Pool({
 	}
 });
 
-express()
+const server=express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const io = socketIO(server);
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
