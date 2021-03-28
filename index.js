411206656f2a3299e74dd58e0e0d802d054ae489
Mabbus44 <rasun44@hotmail.com>
2021-03-28 20:58:03 +0200

    templates

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+console.log('index.js started')
 require('dotenv').config({path:__dirname+'/.env'})
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
-const PORT = process.env.PORT || 3000;
-const { Pool } = require('pg');
+const express = require('express')
+const socketIO = require('socket.io')
+const path = require('path')
+const PORT = process.env.PORT || 3000
+const { Pool } = require('pg')
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
 	ssl: {
 		rejectUnauthorized: false
 	}
-});
+})
 
 const server=express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -18,10 +19,11 @@ const server=express()
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-const io = socketIO(server);
+const io = socketIO(server)
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+  console.log('Client connected')
+  socket.on('disconnect', () => console.log('Client disconnected'))
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000)
+console.log('index.js finished')
