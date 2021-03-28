94c79dfd84885c09a359fbb1782f75db1489e72a
Mabbus44 <rasun44@hotmail.com>
2021-03-28 21:31:00 +0200

    socked id

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const server=express()
 
 const io = socketIO(server)
 io.on('connection', (socket) => {
-  console.log('Client connected')
-  socket.on('disconnect', () => console.log('Client disconnected'))
+  console.log(`Client connected ${socket.id}`)
+  socket.on('disconnect', () => console.log(`Client disconnected ${socket.id}`))
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000)
